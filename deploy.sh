c69d5322a94e222a5666559701ae92d2c227a5f1
Alexander Zhidkov <azhidkov@gmail.com>
2016-04-02 14:01:53 +0800

    updated travis

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -ev
+
+if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+  firebase deploy --token $FIREBASE_TOKEN_DEV
+fi
+
+if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+  firebase deploy --token $FIREBASE_TOKEN
+fi
