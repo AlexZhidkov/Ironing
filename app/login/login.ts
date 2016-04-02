aa276bb3f5c3063c6c9590bbc6ff5c778faa81ff
Alexander Zhidkov <azhidkov@gmail.com>
2016-04-03 07:42:39 +0800

    added tslint

--- app/login/login.ts
+++ app/login/login.ts
@@ -1,75 +1,86 @@
 "use strict";
 angular.module('myApp.login', ['firebase.utils', 'firebase.auth', 'ngRoute'])
-    .config(['$routeProvider', function ($routeProvider) {
+
+    .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/login', {
             controller: 'LoginCtrl',
             templateUrl: 'login/login.html'
         });
     }])
-    .controller('LoginCtrl', ['$scope', 'Auth', '$location', 'fbutil', function ($scope, Auth, $location, fbutil) {
+
+    .controller('LoginCtrl', ['$scope', 'Auth', '$location', 'fbutil', function($scope, Auth, $location, fbutil) {
         $scope.email = null;
         $scope.pass = null;
         $scope.confirm = null;
         $scope.createMode = false;
-        $scope.login = function (email, pass) {
+
+        $scope.login = function(email, pass) {
             $scope.err = null;
             Auth.$authWithPassword({ email: email, password: pass }, { rememberMe: true })
-                .then(function () {
-                $location.path('/account');
-            }, function (err) {
-                $scope.err = errMessage(err);
-            });
+                .then(function(/* user */) {
+                    $location.path('/account');
+                }, function(err) {
+                    $scope.err = errMessage(err);
+                });
         };
-        $scope.createAccount = function () {
+
+        $scope.createAccount = function() {
             $scope.err = null;
             if (assertValidAccountProps()) {
                 var email = $scope.email;
                 var pass = $scope.pass;
+                // create user credentials in Firebase auth system
                 Auth.$createUser({ email: email, password: pass })
-                    .then(function () {
-                    return Auth.$authWithPassword({ email: email, password: pass });
-                })
-                    .then(function (user) {
-                    var ref = fbutil.ref('users', user.uid);
-                    return fbutil.handler(function (cb) {
-                        ref.set({ email: email, name: name || firstPartOfEmail(email) }, cb);
+                    .then(function() {
+                        // authenticate so we have permission to write to Firebase
+                        return Auth.$authWithPassword({ email: email, password: pass });
+                    })
+                    .then(function(user) {
+                        // create a user profile in our data store
+                        var ref = fbutil.ref('users', user.uid);
+                        return fbutil.handler(function(cb) {
+                            ref.set({ email: email, name: name || firstPartOfEmail(email) }, cb);
+                        });
+                    })
+                    .then(function(/* user */) {
+                        // redirect to the account page
+                        $location.path('/account');
+                    }, function(err) {
+                        $scope.err = errMessage(err);
                     });
-                })
-                    .then(function () {
-                    $location.path('/account');
-                }, function (err) {
-                    $scope.err = errMessage(err);
-                });
             }
         };
-        $scope.signInWithProvider = function (provider) {
+
+        $scope.signInWithProvider = function(provider) {
             $scope.err = null;
-            Auth.$authWithOAuthPopup(provider, function (error, authData) {
+            Auth.$authWithOAuthPopup(provider, function(error, authData) {
                 if (error) {
                     $scope.err = errMessage(error);
-                }
-                else {
+                } else {
                     $location.path('/account');
                 }
             });
-        };
-        $scope.signInAsGuest = function () {
+        }
+
+        $scope.signInAsGuest = function() {
             $scope.err = null;
-            Auth.$authAnonymously(function (error) {
+            Auth.$authAnonymously(function(error) {
                 if (error) {
                     $scope.err = errMessage(error);
-                }
-                else {
+                } else {
                     $location.path('/account');
                 }
             });
-        };
-        $scope.callPhone = function () {
+        }
+
+        $scope.callPhone = function() {
             window.location.href = 'tel:+61406522097';
-        };
-        $scope.sendSms = function () {
+        }
+
+        $scope.sendSms = function() {
             window.location.href = 'sms:+61406522097';
-        };
+        }
+
         function assertValidAccountProps() {
             if (!$scope.email) {
                 $scope.err = 'Please enter an email address';
@@ -82,16 +93,19 @@ angular.module('myApp.login', ['firebase.utils', 'firebase.auth', 'ngRoute'])
             }
             return !$scope.err;
         }
+
         function errMessage(err) {
             return angular.isObject(err) && err.code ? err.code : err + '';
         }
+
         function firstPartOfEmail(email) {
             return ucfirst(email.substr(0, email.indexOf('@')) || '');
         }
+
         function ucfirst(str) {
+            // inspired by: http://kevin.vanzonneveld.net
             str += '';
             var f = str.charAt(0).toUpperCase();
             return f + str.substr(1);
         }
     }]);
-//# sourceMappingURL=login.js.map
