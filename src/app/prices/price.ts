651d9da2485410b64d6647424e5dc8f665eb9807
Alexander Zhidkov <azhidkov@gmail.com>
2016-04-12 21:50:35 +0800

    href, price.ts

---
+++ src/app/prices/price.ts
@@ -0,0 +1,14 @@
+export interface IPrice {
+    name: string;
+    price: number;
+}
+
+export class Price implements IPrice {
+    name: string;
+    price: number;
+
+    constructor(name: string, price: number) {
+        this.name = name;
+        this.price = price;
+    }
+}
