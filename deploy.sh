fe354de11fdea94fe3d3f95caba3ca9904a53383
Alex Zhidkov <AlexZhidkov@users.noreply.github.com>
2016-04-12 14:45:39 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -ev
-echo "inside deploy"
+
 if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
   echo "about to firebase deploy dev"
   firebase deploy --token $FIREBASE_TOKEN_DEV
